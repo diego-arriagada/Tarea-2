6afb2430afde02effe9cf5ac37933e349d232807
unknown <mcatril2018@udec.cl>
2025-05-17 20:30:08 -0400

    Implementacion de metodos y getters en la clase Reunion

--- src/main/java/org/tarea2/Reunion.java
+++ src/main/java/org/tarea2/Reunion.java
@@ -16,6 +16,8 @@ abstract class Reunion {
     private ArrayList<Invitable> invitados = new ArrayList<>();
     private ArrayList<Asistencia> asistencias = new ArrayList<>();
     private ArrayList<String> notas = new ArrayList<>();
+    private ArrayList<Asistencia> ausencias = new ArrayList<>();
+    private ArrayList<Asistencia> atrasos = new ArrayList<>();
 
     public Reunion(Instant fecha, Instant horaPrevista, Duration duracionPrevista, TipoReunion tipo, Empleado organizador) {
         this.fecha = fecha;
@@ -31,6 +33,46 @@ abstract class Reunion {
     public void agregarNota(String nota) {
         notas.add(nota);
     }
-
-    // faltan todos los metodos y getters
+    public ArrayList<Asistencia> obtenerAsistencia(){
+        return asistencias;
+    }
+    public ArrayList<Asistencia> obtenerAusencias(){
+        return ausencias;
+    }
+    public ArrayList<Asistencia> obtenerAtrasos(){
+        return atrasos;
+    }
+    public int obtenerTotalAsistencia(){
+        return asistencias.size();
+    }
+    public float obtenerPorcentajeAsistencia(){
+        return (float) asistencias.size() /invitados.size() * 100;
+    }
+    public float calcularTiempoReal(){
+        Duration tiempoReunion = Duration.between(horaInicio, horaFin);
+        return tiempoReunion.toSeconds();
+        //return (float) tiempoReunion.toSeconds()/3600; Opcion para retornar en formato de horas la duracion total para que tenga sentido que retorne un float
+    }
+    public void iniciar(){
+        this.horaInicio = Instant.now();
+    }
+    public void finalizar(){
+        this.horaFin = Instant.now();
+    }
+    public Instant getFecha() {
+        return fecha;
+    }
+    public Instant getHoraPrevista() {
+        return horaPrevista;
+    }
+    public Duration getDuracionPrevista(){
+        return duracionPrevista;
+    }
+    public Instant getHoraInicio() {
+        return horaInicio;
+    }
+    public Instant getHoraFin() {
+        return horaFin;
+    }
+    
 }
